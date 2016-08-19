ada8372360428ed9d09be798773be9ca5809d2bf
kengardiner <ken_gardiner@yahoo.com>
2016-08-19 18:22:08 -0400

    First Push

---
+++ app/src/main/java/com/example/android/coolcalc/CalcActivity.java
@@ -0,0 +1,41 @@
+package com.example.android.coolcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class CalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+
+        Button oneBtn = (Button)findViewById(R.id.oneBtn); //create button variable, call function findviewbyid
+                                                           // cast into (Button) type
+        Button twoBtn = (Button)findViewById(R.id.twoBtn);
+        Button threeBtn = (Button)findViewById(R.id.threeBtn);
+        Button fourBtn = (Button)findViewById(R.id.fourBtn);
+        Button fiveBtn = (Button)findViewById(R.id.fiveBtn);
+        Button sixBtn = (Button)findViewById(R.id.sixBtn);
+        Button sevenBtn = (Button)findViewById(R.id.sevenBtn);
+        Button eightBtn = (Button)findViewById(R.id.eightBtn);
+        Button nineBtn = (Button)findViewById(R.id.nineBtn);
+        Button zeroBtn = (Button)findViewById(R.id.zeroBtn);
+        Button twoBtn = (Button)findViewById(R.id.twoBtn);
+
+        ImageButton calcBtn = (ImageButton)findViewById(R.id.calcBtn);
+        TextView resultsView = (TextView)findViewById(R.id.resultsText);
+
+        oneBtn.setOnClickListener(new View.OnClickListener(){ //creating a class
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+
+    }
+}
